2fa5b0cbefa85d0e477fc5f7111158ebe260a2e4
Phuwasit <phuwasit.n@kkumail.com>
2022-12-02 17:07:48 +0700

    java-exercise

---
+++ Myself/AboutMySelf.java
@@ -0,0 +1,14 @@
+class AboutMySelf{
+    public static void main(String[] args) {
+        String name = "Phuwasit Nuemaihom";
+        double gpa = 3.82;
+        boolean likeProgramming = true;
+        System.out.println("My name is " + name + " who has GPA as " + gpa);
+        if(likeProgramming == true){
+            System.out.println("I like Programming");
+        }
+        else{
+            System.out.println("I don't like Programming");
+        }
+    }
+}
